f9c67eac1ce73dff6459e2bfbc0721a17dd80248
Robert Peters <RobertPeters@camasit.nl>
2025-07-15 16:56:20 +0200

    work on hasm

--- src/Hasm/Components/StateMachineStateNodeModel.cs
+++ src/Hasm/Components/StateMachineStateNodeModel.cs
@@ -1,14 +1,17 @@
 ﻿using Blazor.Diagrams.Core.Geometry;
 using Blazor.Diagrams.Core.Models;
+using Hasm.Models;
 
 namespace Hasm.Components;
 
 public class StateMachineStateNodeModel : NodeModel
 {
-    public StateMachineStateNodeModel(Guid stateId, Point? position = null) : base(position) 
+    public StateMachineStateNodeModel(Guid stateId, StateMachine stateMachine, Point? position = null) : base(position) 
     {
         StateId = stateId;
+        StateMachine = stateMachine;
     }
 
     public Guid StateId { get; set; }
+    public StateMachine StateMachine { get; set; }
 }
